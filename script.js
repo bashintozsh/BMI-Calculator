0158cd4c2b56491965461aef539cf9bca37911e4
Ziya Sharifli <angelandthestar@gmail.com>
2024-01-02 14:27:03 +0300

    Added metric and imperial buttons, and added to check if person is healthy or nott

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
 'use strict';
 
-let age, height, weight, result;
+let age, height, heightImperial, weight, result, health;
 
 // BMI Formula (Metric)
 function bmiMetric(weight, height) {
-  const bmiMetric = Math.trunc(weight / height ** 2);
+  const bmiMetric = Math.trunc(weight / height ** 2).toFixed(2);
   return bmiMetric;
 }
+
+// BMI Formula (Imperial)
+function bmiImperial(weight, height) {
+  height = height * 12 + heightImperial;
+  const bmiImperial = ((weight / height ** 2) * 703).toFixed(2);
+  return bmiImperial;
+}
+
+// isChecked function
+function isChecked(element) {
+  const isChecked = document.querySelector(`.${element}`).checked;
+  return isChecked;
+}
+
+// Imperial event
+
+document.querySelector('.unit-radio-2').addEventListener('click', function () {
+  if (isChecked(`unit-radio-2`)) {
+    document.querySelector('.input-height').textContent =
+      'Your height (in feet and inches)';
+    document.querySelector('.input-weight').textContent =
+      'Your weight: (in lbs)';
+    document
+      .querySelector('.flex-input-height-imperial')
+      .classList.remove('hidden');
+    document.querySelector('.flex-input-weight').value = '';
+    document.querySelector('.flex-input-height').value = '';
+    document.querySelector('.flex-input-height-imperial').value = '';
+  }
+});
+
+// Metric event
+
+document.querySelector('.unit-radio-1').addEventListener('click', function () {
+  if (isChecked(`unit-radio-1`)) {
+    document.querySelector('.input-height').textContent =
+      'Your height (in x.xx cm):';
+    document.querySelector('.input-weight').textContent =
+      'Your weight: (in kg)';
+    document
+      .querySelector('.flex-input-height-imperial')
+      .classList.add('hidden');
+    document.querySelector('.flex-input-weight').value = '';
+    document.querySelector('.flex-input-height').value = '';
+  }
+});
 
 document.querySelector('.result').classList.add('hidden');
 // Submit button event listener
@@ -14,22 +60,45 @@ document.querySelector('.btn--submit').addEventListener('click', function () {
   // Gets values from user (age, height, weight)
   age = Number(document.querySelector('.flex-input-age').value);
   height = Number(document.querySelector('.flex-input-height').value);
+  heightImperial = Number(
+    document.querySelector('.flex-input-height-imperial').value
+  );
+
   weight = Number(document.querySelector('.flex-input-weight').value);
 
   // Calculates the BMI using the user values
-  result = bmiMetric(weight, height);
+  if (isChecked(`unit-radio-1`)) {
+    result = bmiMetric(weight, height);
+  } else if (isChecked(`unit-radio-2`)) {
+    result = bmiImperial(weight, height);
+    console.log(height);
+  }
 
-  // Checks if any of the input fields were left empty, if it was, throws an error to user.
+  // Healthiness checker
+
+  if (result < 18.5) {
+    health = 'underweight';
+  } else if (result > 18.5 && result < 24.9) {
+    health = 'healthy';
+  } else if (result > 25 && result < 29.9) {
+    health = 'overweight';
+  } else if (result > 30 && result < 34.9) {
+    health = 'obese';
+  } else {
+    health = 'morbidly obese';
+  }
+
   if (!age || !height || !weight) {
+    // Checks if any of the input fields were left empty, if it was, throws an error to user.
     document.querySelector('.result').classList.remove('hidden');
     document.querySelector(
       '.result'
-    ).textContent = `Error: You missed on of the inputs.`;
+    ).textContent = `Error: You missed on one of the inputs.`;
   } else {
     // prints final result
     document.querySelector('.result').classList.remove('hidden');
     document.querySelector(
       '.result'
-    ).textContent = `Your BMI result is ${result}.`;
+    ).textContent = `Your BMI result is ${result}, which is ${health}.`;
   }
 });
