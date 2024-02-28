f2ed08f3c46fe193cad579296ed29552c66d1428
Ziya Sharifli <angelandthestar@gmail.com>
2024-02-28 22:40:43 +0300

    Add history, risk color and bug fixes.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 let age, height, heightImperial, weight, result, health;
+let history = [];
+let count = 1;
 
 // BMI Formula (Metric)
 function bmiMetric(weight, height) {
@@ -57,9 +59,17 @@ document.querySelector('.unit-radio-1').addEventListener('click', function () {
 document.querySelector('.result').classList.add('hidden');
 // Submit button event listener
 document.querySelector('.btn--submit').addEventListener('click', function () {
+  // Reset indicators
+
+  document.querySelector('.obese').classList.add('hidden');
+  document.querySelector('.underweight').classList.add('hidden');
+  document.querySelector('.healthy').classList.add('hidden');
+  document.querySelector('.overweight').classList.add('hidden');
+
   // Gets values from user (age, height, weight)
   age = Number(document.querySelector('.flex-input-age').value);
   height = Number(document.querySelector('.flex-input-height').value);
+
   heightImperial = Number(
     document.querySelector('.flex-input-height-imperial').value
   );
@@ -78,15 +88,54 @@ document.querySelector('.btn--submit').addEventListener('click', function () {
 
   if (result < 18.5) {
     health = 'underweight';
+    document
+      .querySelector('.flexbox-item-indicator')
+      .classList.remove('hidden');
+    document.querySelector('.underweight').classList.remove('hidden');
   } else if (result > 18.5 && result < 24.9) {
     health = 'healthy';
+    document
+      .querySelector('.flexbox-item-indicator')
+      .classList.remove('hidden');
+    document.querySelector('.healthy').classList.remove('hidden');
   } else if (result > 25 && result < 29.9) {
     health = 'overweight';
-  } else if (result > 30 && result < 34.9) {
+    document
+      .querySelector('.flexbox-item-indicator')
+      .classList.remove('hidden');
+    document.querySelector('.overweight').classList.remove('hidden');
+  } else if (result > 30) {
     health = 'obese';
-  } else {
-    health = 'morbidly obese';
+    document
+      .querySelector('.flexbox-item-indicator')
+      .classList.remove('hidden');
+    document.querySelector('.obese').classList.remove('hidden');
   }
+
+  // History logic
+
+  const table = document.querySelector('table');
+  const row = table.insertRow();
+
+  const td0 = row.insertCell(0);
+  const td1 = row.insertCell(1);
+  const td2 = row.insertCell(2);
+  const td3 = row.insertCell(3);
+  const td4 = row.insertCell(4);
+  const td5 = row.insertCell(5);
+  let temphistory = [];
+
+  history.push(age, height, weight, result, health);
+
+  td0.innerText = count;
+  td1.innerText = history[0];
+  td2.innerText = history[1];
+  td3.innerText = history[2];
+  td4.innerText = history[3];
+  td5.innerText = history[4];
+
+  history = [];
+  count++;
 
   if (!age || !height || !weight) {
     // Checks if any of the input fields were left empty, if it was, throws an error to user.
@@ -101,4 +150,16 @@ document.querySelector('.btn--submit').addEventListener('click', function () {
       '.result'
     ).textContent = `Your BMI result is ${result}, which is ${health}.`;
   }
+});
+
+// History button
+
+document.querySelector('.btn--history').addEventListener('click', function () {
+  document.querySelector('.modal').classList.remove('hidden');
+});
+
+// Modal close button
+
+document.querySelector('.btn--close').addEventListener('click', function () {
+  document.querySelector('.modal').classList.add('hidden');
 });
